6f1bb4cd1f43484405f78035956fde5f510c8632
Eric <liaogd79@gmail.com>
2025-02-20 17:28:26 +0800

    💡 feat: gh page

--- docs/current/payment-service/mobile-payment/index.md
+++
@@ -1,9 +0,0 @@
----
-title: 行動支付
-sidebar_position: 2
----
-# 行動支付
-
-PayNow 提供以下的行動支付金流服務，讓您收款更便利。
-- [Apple Pay](./apple-pay)
-- [icash Pay](./icash-pay)
--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Redirect } from '@docusaurus/router';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-
-export default function Home() {
-  const to = useBaseUrl('/docs/')
-  return <Redirect to={to} />;
-}
